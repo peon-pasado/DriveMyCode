e4dd67506bceb0ae096e392bf0a40aaba794b2f4
Miguel Alessandro Mini H. <45865868+peon-pasado@users.noreply.github.com>
2024-10-16 12:27:59 -0300

    astro?

--- astro.config.mjs
+++ astro.config.mjs
@@ -31,8 +31,8 @@ const oklchToHex = (str) => {
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://fuwari.vercel.app/",
-  base: "/",
+  site: "https://peon-pasado.github.io",
+  base: "DriveMyCode",
   trailingSlash: "always",
   integrations: [
     tailwind(),
